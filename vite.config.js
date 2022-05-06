5a35178dff7c384c3107aca7a19ee7cbfeeecd71
TKadyear <tamarakadyear@gmail.com>
2022-05-06 08:53:45 +0200

    Added routes to the vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,28 @@
 const path = require("path");
+const { resolve } = require('path');
+const { defineConfig } = require('vite');
 const mode = process.env.NODE_ENV === "production" ? "production" : "development";
 const base = mode === "production" ? "/" + path.basename(process.cwd()) + "/" : "/";   // GitHub
 // const base = "/"; // Netlify
 
-module.exports = {
+module.exports = defineConfig({
   root: "src",
   base,
   mode,
   publicDir: "../public",
   build: {
     outDir: "../dist",
-    assetsDir: "./"
+    assetsDir: "./",
+    rollupOptions: {
+      input: {
+        main: resolve("src", 'index.html'),
+        home: resolve("src", 'home.html'),
+        about: resolve("src", 'about.html'),
+        contact: resolve("src", 'contact.html'),
+        offers: resolve("src", 'offers.html'),
+        rooms: resolve("src", 'rooms.html'),
+        roomDetails: resolve("src", 'room-details.html')
+      }
+    }
   }
-};
+});
